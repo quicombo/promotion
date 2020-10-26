8101ad0bda2e8af2c4eb4523f9e8301ad3b66ea6
Арсений Александрович <quicombo@gmail.com>
2020-10-26 07:13:02 +0300

    fxs

--- src/views/About.vue
+++ src/views/About.vue
@@ -74,54 +74,24 @@
   gsap.registerPlugin(ScrollTrigger);
   export default  {
       name: 'about',
-      mounted() {
-          this.startAnimation();
-      },
       methods: {
-          animateFrom(elem, direction) {
-              direction = direction | 1;
-              let x = 0;
-              let y = direction * 100;
-              console.log(elem);
-              if(elem.classList.contains('from_left')) {
-                  x = -100;
-                  y = 0
-              } else
-              if (elem.classList.contains('from_right')) {
-                  x = 100;
-                  y = 0;
-              }
-              gsap.fromTo(elem, {
-                  x: x,
-                  y: y,
-                  autoAlpha: 0
-              }, {
-                  duration: 1.25,
-                  x: 0,
-                  y: 0,
-                  autoAlpha: 1,
-                  ease: "expo",
-                  overwrite: "auto"
-              });
-          },
-          hide(elem) {
-              gsap.set(elem, {autoAlpha: 0});
-          },
-          startAnimation() {
-
-              //gsap.registerPlugin(ScrollTrigger)
-              gsap.utils.toArray('.reveal').forEach((elem) => {
-                  gsap.set(elem, {autoAlpha: 0});
-                  ScrollTrigger.create({
-                      markers:false,
-                      trigger: elem,
-                      toggleActions: "play none reverse none",
-                      onEnter: this.animateFrom(elem),
-                      onEnterBack: this.animateFrom(elem, -2),
-                      onLeave: this.hide(elem)
+          onscroll() {
+              gsap.utils.toArray(".reveal").forEach(anim => {
+                  gsap.from(anim, {
+                      scrollTrigger: {
+                          start: "top bottom",
+                          end: "top center",
+                          scrub: 1,
+                          trigger: anim
+                      },
+                      y: 120,
+                      opacity: 0.1
                   })
               })
           }
+      },
+      mounted() {
+          this.onscroll()
       }
 
   }
